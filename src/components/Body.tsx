59b14f6f95d54c4b31ed58795d24051c0a14d772
Gabriel Barreto <gh.barreto@hotmail.com>
2022-11-08 16:57:27 -0800

    dark mode working

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,25 +1,14 @@
 import { useStore } from '@nanostores/react';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 import { isDarkMode } from '../themeMode';
 
-const queryClient = new QueryClient();
-
 export const Body = (props: any) => {
     const $isDarkMode = useStore(isDarkMode);
-
-    const { isLoading, error, data } = useQuery({
-        queryKey: ['repoData'],
-        queryFn: () => fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res => res.json()),
-    });
-
     return (
-        <QueryClientProvider client={queryClient}>
-            <div className={$isDarkMode ? 'dark' : ''}>
-                <div slot="body">{props.body}</div>
-                asdasdasd
-                {isLoading ? 'Loading' : 'loaded'}
+        <div className={$isDarkMode ? 'dark' : 'white'}>
+            <div slot="body" className={'bg-slate-400 dark:bg-slate-900'}>
+                {props.body}
             </div>
-        </QueryClientProvider>
+        </div>
     );
 };
--- src/components/ToggleMode.tsx
+++ src/components/ToggleMode.tsx
@@ -3,9 +3,13 @@ import { isDarkMode } from '../themeMode';
 
 export const ToggleMode = () => {
     const $isDarkMode = useStore(isDarkMode);
+
     return (
-        <main>
-            <button onClick={() => isDarkMode.set(!$isDarkMode)}>Toggle</button>
-        </main>
+        <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => isDarkMode.set(!$isDarkMode)}
+        >
+            Toggle
+        </button>
     );
 };
