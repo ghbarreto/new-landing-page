8246a59736d6d87be2a3b8c3b89d5ac29ecf0e52
Gabriel Barreto <gh.barreto@hotmail.com>
2023-05-16 16:03:39 -0700

    route

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send({ text: 'worked' });
+    res.send({ text: 'api route' });
 });
 
 app.get('/api/contributions', async (_req, res) => {
